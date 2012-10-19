ac1f59cab9bf565a2517baa32330ad905cbf987c
Maël Nison <nison.mael@gmail.com>
2012-10-19 10:37:23 -0400

    First commit

---
+++ bootlist.php
@@ -0,0 +1,72 @@
+<?php
+    function getIcon( $mime ) {
+        $icons = array(
+            'video/.*' => 'icon-film',
+            'audio/.*' => 'icon-music',
+            'image/.*' => 'icon-picture',
+        );
+
+        foreach ( $icons as $regexp => $icon ) {
+            if ( preg_match('#^' . $regexp . '$#', $mime ) ) {
+                return $icon;
+            }
+        }
+
+        return 'icon-file';
+    }
+
+    define( 'BOOTLIST_ROOT', dirname( $_SERVER['SCRIPT_NAME'] ) );
+
+    $directories = array( );
+    $entries = array( );
+
+    $fileinfo = new FInfo( FILEINFO_MIME, '/usr/share/misc/magic.mgc' );
+    $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . $_SERVER[ 'REQUEST_URI' ];
+    if ( ( $folder = opendir( $path ) ) === false) die( 'Unexpected error' );
+    while ( ( $name = readdir( $folder ) ) !== false ) :
+        if ( $name[ 0 ] !== '.' || $name === '..' ) :
+            $entryPath = $path . DIRECTORY_SEPARATOR . $name;
+            if ( is_dir( $entryPath ) ) :
+                $directories[ ] = $name;
+            else :
+                $entries[ $fileinfo->file( $entryPath ) ][ ] = $name;
+            endif ;
+        endif ;
+    endwhile ;
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="<?php echo BOOTLIST_ROOT; ?>/bootstrap/css/bootstrap.min.css" />
+    </head>
+    <body>
+        <h1 style="margin-left: 10px;"><?php echo htmlentities( $_SERVER[ 'REQUEST_URI' ] ) ?></h1>
+        <table class="table" data-provides="rowlink">
+            <colgroup>
+                <col width="40" />
+                <col width="*" />
+            </colgroup>
+            <tr>
+                <th></th>
+                <th>Entry name</th>
+            </tr>
+            <?php foreach ( $directories as $directory ): ?>
+                <tr>
+                    <td><i class="icon-folder-close"></i></td>
+                    <td><a href="<?php echo htmlentities( $directory, ENT_QUOTES ); ?>"><?php echo htmlentities( $directory ); ?></a></td>
+                </tr>
+            <?php endforeach ?>
+            <?php foreach ( $entries as $type => $list ) : ?>
+                <?php $icon = getIcon( $type ); ?>
+                <?php foreach ( $list as $entry ) : ?>
+                    <tr>
+                        <td><i class="<?php echo $icon; ?>"></i></td>
+                        <td><a href="<?php echo htmlentities( $entry, ENT_QUOTES ); ?>"><?php echo htmlentities( $entry ) ?></a></td>
+                    </tr>
+                <?php endforeach ?>
+            <?php endforeach ?>
+        </table>
+    </body>
+</html>
